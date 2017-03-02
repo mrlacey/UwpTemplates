1cad270d03c151afa84eb362e56ddb2e86244973
Matt Lacey <matt@mrlacey.co.uk>
2017-03-02 21:32:02 +0000

    Quick Test App
    An app template for quickly testing something in an app. It's just a
    blank app with a button already hooked up.

---
+++ UWPQuickTestApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace $safeprojectname$
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TestSomethingClicked(object sender, RoutedEventArgs e)
+        {
+            // TODO: do testing here
+        }
+    }
+}
